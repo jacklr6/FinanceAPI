3d54888c23c04b8534250a7b05691866a6d1884d
Jack Rogers <135053731+jacklr6@users.noreply.github.com>
2025-07-19 23:03:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,32 @@ def get_stock_price():
         ticker = yf.Ticker(symbol)
         info = ticker.info
 
+        # Historical data (last 1 day)
+        hist = ticker.history(period='1d')
+        last_close = hist['Close'].iloc[-1] if not hist.empty else None
+
+        # Expanded data
         data = {
             'symbol': symbol.upper(),
             'current_price': info.get('regularMarketPrice'),
+            'previous_close': info.get('previousClose'),
+            'open': info.get('open'),
             'daily_high': info.get('dayHigh'),
             'daily_low': info.get('dayLow'),
+            'volume': info.get('volume'),
+            'market_cap': info.get('marketCap'),
             'pe_ratio': info.get('trailingPE'),
-            'timestamp': info.get('regularMarketTime')
+            'eps': info.get('trailingEps'),
+            'dividend_yield': info.get('dividendYield'),
+            '52_week_high': info.get('fiftyTwoWeekHigh'),
+            '52_week_low': info.get('fiftyTwoWeekLow'),
+            'currency': info.get('currency'),
+            'exchange': info.get('exchange'),
+            'sector': info.get('sector'),
+            'industry': info.get('industry'),
+            'website': info.get('website'),
+            'timestamp': info.get('regularMarketTime'),
+            'last_close_from_history': last_close
         }
 
         return jsonify(data)
